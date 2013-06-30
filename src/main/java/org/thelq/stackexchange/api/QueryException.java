d4dfb9df7cf8a075fa7663d743dd20a04edaf97a
Leon Blakey <lord.quackstar@gmail.com>
2013-06-30 08:32:56 -0400

    Add getters

--- src/main/java/org/thelq/stackexchange/api/QueryException.java
+++ src/main/java/org/thelq/stackexchange/api/QueryException.java
@@ -5,21 +5,26 @@
 package org.thelq.stackexchange.api;
 
 import java.net.URI;
+import lombok.Getter;
 
 /**
  *
  * @author Leon
  */
+@Getter
 public class QueryException extends RuntimeException {
 	protected final URI uri;
+	protected final String rawResponse;
 
 	public QueryException(URI uri, String rawResponse, String message) {
 		super(message + "\nURI: " + uri);
 		this.uri = uri;
+		this.rawResponse = rawResponse;
 	}
 
 	public QueryException(URI uri, String rawResponse, String message, Throwable cause) {
 		super(message + "\nURI: " + uri, cause);
 		this.uri = uri;
+		this.rawResponse = rawResponse;
 	}
 }
