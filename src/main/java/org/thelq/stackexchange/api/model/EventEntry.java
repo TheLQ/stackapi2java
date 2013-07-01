edb911f0102c7534be2101e661c8cb2f2ec67662
Leon Blakey <lord.quackstar@gmail.com>
2013-06-30 21:28:01 -0400

    Added support for events

---
+++ src/main/java/org/thelq/stackexchange/api/model/EventEntry.java
@@ -0,0 +1,31 @@
+/**
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thelq.stackexchange.api.model;
+
+import java.net.URI;
+import lombok.Getter;
+import org.joda.time.DateTime;
+
+/**
+ *
+ * @author Leon
+ */
+@Getter
+public class EventEntry implements GenericEntry {
+	protected DateTime creationDate;
+	protected int eventId;
+	protected Type eventType;
+	@MaybeAbsent
+	protected String Excerpt;
+	protected URI link;
+
+	public static enum Type {
+		QUESTION_POSTED,
+		ANSWER_POSTED,
+		COMMENT_POSTED,
+		POST_EDITED,
+		USER_CREATED;
+	}
+}
