bd3061fb9577803e37c4fe77db9ba4e07386fa30
Leon Blakey <lord.quackstar@gmail.com>
2013-07-14 11:56:37 -0400

    New vector required parameter

--- src/main/java/org/thelq/stackexchange/api/queries/methods/VectorQueryMethod.java
+++ src/main/java/org/thelq/stackexchange/api/queries/methods/VectorQueryMethod.java
@@ -4,6 +4,7 @@
  */
 package org.thelq.stackexchange.api.queries.methods;
 
+import com.google.common.base.Preconditions;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.thelq.stackexchange.api.queries.QueryUtils;
@@ -12,13 +13,23 @@ import org.thelq.stackexchange.api.queries.QueryUtils;
  *
  * @author Leon
  */
-@RequiredArgsConstructor
 @Getter
 public class VectorQueryMethod implements QueryMethod {
 	protected final String raw;
-	protected final Iterable<?> iterator;
+	protected final Iterable<?> vector;
+	protected final boolean vectorRequired;
+
+	public VectorQueryMethod(String raw, Iterable<?> vector) {
+		this(raw, vector, false);
+	}
+
+	public VectorQueryMethod(String raw, Iterable<?> vector, boolean vectorRequired) {
+		this.raw = raw;
+		this.vector = vector;
+		this.vectorRequired = vectorRequired;
+	}
 
 	public String getFinal() {
-		return QueryUtils.insertVector(raw, iterator);
+		return QueryUtils.insertVector(raw, vector);
 	}
 }
