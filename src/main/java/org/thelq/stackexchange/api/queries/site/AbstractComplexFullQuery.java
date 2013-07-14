74341851196e8e0bf139e1a27a5b0a03e9e18db4
Leon Blakey <lord.quackstar@gmail.com>
2013-07-14 14:29:36 -0400

    Made DateOrder sorted and moved sort field

--- src/main/java/org/thelq/stackexchange/api/queries/site/AbstractComplexFullQuery.java
+++ src/main/java/org/thelq/stackexchange/api/queries/site/AbstractComplexFullQuery.java
@@ -16,20 +16,12 @@ import org.thelq.stackexchange.api.queries.methods.QueryMethod;
  * @author Leon
  */
 @Getter
-public class AbstractComplexFullQuery<F extends Enum<F> & SortableField, Q extends AbstractComplexFullQuery<F, Q, I>, I extends ItemEntry> extends AbstractComplexOrderDateQuery<Q, I> {
-	protected final Class<F> enumClass;
-	protected F sort;
+public class AbstractComplexFullQuery<F extends Enum<F> & SortableField, Q extends AbstractComplexFullQuery<F, Q, I>, I extends ItemEntry> extends AbstractComplexSortedQuery<F, Q, I> {
 	protected Integer min;
 	protected Integer max;
 
 	public AbstractComplexFullQuery(Class<F> enumClass, Class<I> itemClass, QueryMethod method) {
-		super(itemClass, method);
-		this.enumClass = enumClass;
-	}
-
-	public Q setSort(F sort) {
-		this.sort = sort;
-		return self();
+		super(enumClass, itemClass, method);
 	}
 
 	public Q setMin(int min) {
---
+++ src/main/java/org/thelq/stackexchange/api/queries/site/AbstractComplexSortedQuery.java
@@ -0,0 +1,38 @@
+/**
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thelq.stackexchange.api.queries.site;
+
+import java.util.LinkedHashMap;
+import lombok.Getter;
+import org.thelq.stackexchange.api.model.ItemEntry;
+import org.thelq.stackexchange.api.model.SortableField;
+import org.thelq.stackexchange.api.queries.QueryUtils;
+import org.thelq.stackexchange.api.queries.methods.QueryMethod;
+
+/**
+ *
+ * @author Leon
+ */
+@Getter
+public class AbstractComplexSortedQuery<F extends Enum<F> & SortableField, Q extends AbstractComplexSortedQuery<F, Q, I>, I extends ItemEntry> extends AbstractComplexDateQuery<Q, I> {
+	protected Order order;
+	protected final Class<F> enumClass;
+	protected F sort;
+
+	public AbstractComplexSortedQuery(Class<F> enumClass, Class<I> itemClass, QueryMethod method) {
+		super(itemClass, method);
+		this.enumClass = enumClass;
+	}
+	
+	public Q setSort(F sort) {
+		this.sort = sort;
+		return self();
+	}
+
+	public Q setOrder(Order order) {
+		this.order = order;
+		return self();
+	}
+}
