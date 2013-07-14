7c9e899d90f0ebb63d24294d144a1c2691982e4f
Leon Blakey <lord.quackstar@gmail.com>
2013-07-14 13:46:12 -0400

    Javadoc added for AnswerQueries

--- src/main/java/org/thelq/stackexchange/api/queries/site/AnswerQueries.java
+++ src/main/java/org/thelq/stackexchange/api/queries/site/AnswerQueries.java
@@ -13,20 +13,37 @@ import org.thelq.stackexchange.api.queries.methods.SimpleQueryMethod;
 import org.thelq.stackexchange.api.queries.methods.VectorQueryMethod;
 
 /**
- *
+ * Utility factory for <code>/answers</code> queries
  * @author Leon
  */
 public class AnswerQueries {
+	/**
+	 * Get all answers on the site
+	 * @see <a href="https://api.stackexchange.com/docs/answers">StackExchange API /answers usage documentation</a>
+	 * @return Generated configurable query
+	 */
 	public static <Q extends AbstractComplexFullQuery<AnswerSort, Q, AnswerEntry>> Q all() {
 		return new AbstractComplexFullQuery<AnswerSort, Q, AnswerEntry>(AnswerSort.class, AnswerEntry.class, new SimpleQueryMethod("answers"))
 				.self();
 	}
 
+	/**
+	 * Get answers that have the specified id
+	 * @see <a href="https://api.stackexchange.com/docs/answers-by-ids">StackExchange API /answers/{} usage documentation</a>
+	 * @param answerIds Non-null, non-empty collection of answer ids
+	 * @return Generated configurable query
+	 */
 	public static <Q extends AbstractComplexFullQuery<AnswerSort, Q, AnswerEntry>> Q byIds(Collection<Integer> answerIds) {
 		return new AbstractComplexFullQuery<AnswerSort, Q, AnswerEntry>(AnswerSort.class, AnswerEntry.class, new VectorQueryMethod("answers/{}", answerIds))
 				.self();
 	}
 
+	/**
+	 * Get all comments for the specified answer ids
+	 * @see <a href="https://api.stackexchange.com/docs/comments-on-answers">StackExchange API /answers/{}/comments usage documentation</a>
+	 * @param answerIds Non-null, non-empty collection of answer ids
+	 * @return Generated configurable query
+	 */
 	public static <Q extends AbstractComplexFullQuery<CommentsSort, Q, CommentEntry>> Q comments(Collection<Integer> answerIds) {
 		return new AbstractComplexFullQuery<CommentsSort, Q, CommentEntry>(CommentsSort.class, CommentEntry.class, new VectorQueryMethod("answers/{}/comments", answerIds))
 				.self();
