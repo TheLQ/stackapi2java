5eda803d24962d6ad6a5fffe403ba680764c8aaf
Leon Blakey <lord.quackstar@gmail.com>
2013-07-18 14:00:50 -0400

    Added support for comment queries

---
+++ src/main/java/org/thelq/stackexchange/api/queries/site/CommentQueries.java
@@ -0,0 +1,46 @@
+/**
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thelq.stackexchange.api.queries.site;
+
+import java.util.Collection;
+import lombok.NonNull;
+import org.thelq.stackexchange.api.model.types.CommentEntry;
+import org.thelq.stackexchange.api.queries.AuthRequiredQuery;
+import org.thelq.stackexchange.api.queries.methods.QueryMethod;
+import org.thelq.stackexchange.api.queries.methods.SimpleQueryMethod;
+import org.thelq.stackexchange.api.queries.methods.VectorQueryMethod;
+import org.thelq.stackexchange.api.queries.site.sort.CommentSort;
+
+/**
+ *
+ * @author Leon
+ */
+public class CommentQueries {
+	public static <Q extends AbstractComplexFullQuery<CommentSort<?>, Q, CommentEntry>> Q all() {
+		return new AbstractComplexFullQuery<CommentSort<?>, Q, CommentEntry>(CommentEntry.class, new SimpleQueryMethod("comments"))
+				.self();
+	}
+
+	public static <Q extends AbstractComplexFullQuery<CommentSort<?>, Q, CommentEntry>> Q byIds(@NonNull Collection<Integer> commentIds) {
+		return new AbstractComplexFullQuery<CommentSort<?>, Q, CommentEntry>(CommentEntry.class, new VectorQueryMethod("comments/{}", commentIds))
+				.self();
+	}
+
+	public static <Q extends AbstractSiteQuery<Q, CommentEntry>> Q edit(int commentId, @NonNull String body) {
+		return new CommentWriteQuery<Q>(CommentEntry.class, new VectorQueryMethod("comments/{}/edit", String.valueOf(commentId)))
+				.setParameter("body", body);
+	}
+
+	public static <Q extends AbstractSiteQuery<Q, CommentEntry>> Q delete(int commentId) {
+		return new CommentWriteQuery<Q>(CommentEntry.class, new VectorQueryMethod("comments/{}/delete", String.valueOf(commentId)))
+				.self();
+	}
+
+	protected static class CommentWriteQuery<Q extends AbstractCommentWriteQuery<Q>> extends AbstractSiteQuery<Q, CommentEntry> implements AuthRequiredQuery {
+		public CommentWriteQuery(Class<CommentEntry> itemClass, QueryMethod method) {
+			super(itemClass, method);
+		}
+	}
+}
