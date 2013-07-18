82182c609be0c11166f18f66e86bf71f6349c0ba
Leon Blakey <lord.quackstar@gmail.com>
2013-07-18 13:57:22 -0400

    VectorQueryMethod can now take single parameter vectors

--- src/main/java/org/thelq/stackexchange/api/queries/methods/VectorQueryMethod.java
+++ src/main/java/org/thelq/stackexchange/api/queries/methods/VectorQueryMethod.java
@@ -19,29 +19,45 @@ import org.thelq.stackexchange.api.queries.QueryUtils;
 @Getter
 public class VectorQueryMethod implements QueryMethod {
 	protected final String raw;
-	protected final Collection<?> vector;
+	protected final String vectorSingle;
+	protected final Collection<?> vectorCollection;
 
-	public VectorQueryMethod(@NonNull String raw, @NonNull Collection<?> vector) {
+	public VectorQueryMethod(@NonNull String raw, @NonNull String vectorSingle) {
 		Preconditions.checkArgument(StringUtils.countMatches(raw, "{}") == 1, "Raw method does not contain vector");
-		Preconditions.checkArgument(!vector.isEmpty(), "Vector collection is empty");
-		Preconditions.checkArgument(vector.size() < 100, "Vectors do not support more than 100 elements");
 		this.raw = raw;
-		this.vector = vector;
+		this.vectorSingle = vectorSingle;
+		this.vectorCollection = null;
+	}
+
+	public VectorQueryMethod(@NonNull String raw, @NonNull Collection<?> vectorCollection) {
+		Preconditions.checkArgument(StringUtils.countMatches(raw, "{}") == 1, "Raw method does not contain vector");
+		Preconditions.checkArgument(!vectorCollection.isEmpty(), "Vector collection is empty");
+		Preconditions.checkArgument(vectorCollection.size() < 100, "Vectors do not support more than 100 elements");
+		this.raw = raw;
+		this.vectorSingle = null;
+		this.vectorCollection = vectorCollection;
 	}
 
 	public String getFinal() {
-		//Verify vector if it is required
-		Iterator<?> vectorItr = vector.iterator();
-		if (!vectorItr.hasNext())
-			throw new RuntimeException("Vector cannot be empty");
+		String methodFinal = raw;
+		if (vectorSingle != null)
+			methodFinal = StringUtils.replaceOnce(raw, "{}", vectorSingle);
+		else if (vectorCollection != null) {
+			if (StringUtils.countMatches(raw, "{}") != 1)
+				throw new RuntimeException("No more vectors to replace! Raw: " + raw + " | Final: " + methodFinal);
+			//Verify vector if it is required
+			Iterator<?> vectorItr = vectorCollection.iterator();
+			if (!vectorItr.hasNext())
+				throw new RuntimeException("Vector cannot be empty");
 
-		//Do the replace
-		String methodFinal = StringUtils.replaceOnce(raw, "{}", QueryUtils.PARAMETER_JOINER.join(vectorItr));
+			//Do the replace
+			methodFinal = StringUtils.replaceOnce(raw, "{}", QueryUtils.PARAMETER_JOINER.join(vectorItr));
 
-		//Make sure we didn't miss anything
-		if (methodFinal.contains("{}"))
-			throw new RuntimeException("Still contains vector: " + methodFinal);
-
+			//Make sure we didn't miss anything
+			if (methodFinal.contains("{}"))
+				throw new RuntimeException("Still contains vector: " + methodFinal);
+		} else
+			throw new RuntimeException("No vector to replace! " + raw);
 		return methodFinal;
 	}
 }
