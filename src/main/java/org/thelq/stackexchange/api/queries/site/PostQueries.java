472473650ee6c206073ac8dece7c7d29c735c523
Leon Blakey <lord.quackstar@gmail.com>
2013-07-18 21:18:29 -0400

    Made all PostQueries methods static

--- src/main/java/org/thelq/stackexchange/api/queries/site/PostQueries.java
+++ src/main/java/org/thelq/stackexchange/api/queries/site/PostQueries.java
@@ -22,32 +22,32 @@ import org.thelq.stackexchange.api.queries.site.sort.SuggestedEditSort;
  * @author Leon
  */
 public class PostQueries {
-	public <Q extends AbstractComplexFullQuery<PostSort<?>, Q, PostEntry>> Q all() {
+	public static <Q extends AbstractComplexFullQuery<PostSort<?>, Q, PostEntry>> Q all() {
 		return new AbstractComplexFullQuery<PostSort<?>, Q, PostEntry>(PostEntry.class, new SimpleQueryMethod("posts"))
 				.self();
 	}
 	
-	public <Q extends AbstractComplexFullQuery<PostSort<?>, Q, PostEntry>> Q byIds(@NonNull Collection<Integer> postIds) {
+	public static <Q extends AbstractComplexFullQuery<PostSort<?>, Q, PostEntry>> Q byIds(@NonNull Collection<Integer> postIds) {
 		return new AbstractComplexFullQuery<PostSort<?>, Q, PostEntry>(PostEntry.class, new VectorQueryMethod("posts/{}", postIds))
 				.self();
 	}
 	
-	public <Q extends AbstractComplexFullQuery<CommentSort<?>, Q, CommentEntry>> Q comments(@NonNull Collection<Integer> postIds) {
+	public static <Q extends AbstractComplexFullQuery<CommentSort<?>, Q, CommentEntry>> Q comments(@NonNull Collection<Integer> postIds) {
 		return new AbstractComplexFullQuery<CommentSort<?>, Q, CommentEntry>(CommentEntry.class, new VectorQueryMethod("posts/{}/comments", postIds))
 				.self();
 	}
 	
-	public <Q extends AbstractSiteQuery<Q, CommentEntry>> Q commentAdd(int postId, @NonNull String body) {
+	public static <Q extends AbstractSiteQuery<Q, CommentEntry>> Q commentAdd(int postId, @NonNull String body) {
 		return new AbstractCommentWriteQuery<Q>(CommentEntry.class, new VectorQueryMethod("posts/{}/comments/add", String.valueOf(postId)))
 				.setParameter("body", body);
 	}
 	
-	public <Q extends AbstractComplexDateQuery<Q, RevisionEntry>> Q revisions(@NonNull Collection<Integer> postIds) {
+	public static <Q extends AbstractComplexDateQuery<Q, RevisionEntry>> Q revisions(@NonNull Collection<Integer> postIds) {
 		return new AbstractComplexDateQuery<Q, RevisionEntry>(RevisionEntry.class, new VectorQueryMethod("posts/{}/revisions", postIds))
 				.self();
 	}
 	
-	public <Q extends AbstractComplexFullQuery<SuggestedEditSort<?>, Q, SuggestedEditEntry>> Q suggestedEdits(@NonNull Collection<Integer> postIds) {
+	public static <Q extends AbstractComplexFullQuery<SuggestedEditSort<?>, Q, SuggestedEditEntry>> Q suggestedEdits(@NonNull Collection<Integer> postIds) {
 		return new AbstractComplexFullQuery<SuggestedEditSort<?>, Q, SuggestedEditEntry>(SuggestedEditEntry.class, new VectorQueryMethod("posts/{}/suggested-edits", postIds))
 				.self();
 	}
