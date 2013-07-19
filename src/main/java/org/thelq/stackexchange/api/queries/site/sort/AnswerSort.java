a92675b95edf891e15688bce959639102b10056e
Leon Blakey <lord.quackstar@gmail.com>
2013-07-18 20:46:45 -0400

    Renamed BaseAnswerSort to AnswerSort

---
+++ src/main/java/org/thelq/stackexchange/api/queries/site/sort/AnswerSort.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thelq.stackexchange.api.queries.site.sort;
+
+import org.joda.time.DateTime;
+import org.thelq.stackexchange.api.queries.site.sort.ResultSort;
+
+/**
+ *
+ * @author Leon
+ */
+public class AnswerSort<M> extends ResultSort<M, AnswerSort<M>> {
+	public static AnswerSort<DateTime> activity() {
+		return new AnswerSort<DateTime>("activity");
+	}
+
+	public static AnswerSort<DateTime> creation() {
+		return new AnswerSort<DateTime>("creation");
+	}
+
+	public static AnswerSort<Integer> votes() {
+		return new AnswerSort<Integer>("votes");
+	}
+
+	public AnswerSort(String name) {
+		super(name);
+	}
+}
