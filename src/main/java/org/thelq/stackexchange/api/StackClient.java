c67947f7bca3d75aa4221882dd4418c10d463673
Leon Blakey <lord.quackstar@gmail.com>
2013-08-05 02:00:33 -0400

    Added support for uppercase enums

--- src/main/java/org/thelq/stackexchange/api/StackClient.java
+++ src/main/java/org/thelq/stackexchange/api/StackClient.java
@@ -17,14 +17,27 @@
  */
 package org.thelq.stackexchange.api;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.Module.SetupContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.deser.Deserializers;
+import com.fasterxml.jackson.databind.deser.Deserializers.Base;
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.google.common.base.Preconditions;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Map;
@@ -40,10 +53,10 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.thelq.stackexchange.api.model.ItemEntry;
-import org.thelq.stackexchange.api.model.types.BadgeEntry;
+import org.thelq.stackexchange.api.model.types.AnswerEntry;
 import org.thelq.stackexchange.api.model.types.ResponseEntry;
 import org.thelq.stackexchange.api.queries.AbstractQuery;
-import org.thelq.stackexchange.api.queries.site.UserQueries;
+import org.thelq.stackexchange.api.queries.site.AnswerQueries;
 
 /**
  *
@@ -66,7 +79,19 @@ public class StackClient {
 		jsonMapper.registerModule(new JodaModule());
 		jsonMapper.registerModule(new GuavaModule());
 		jsonMapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
-		jsonMapper.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true);
+		jsonMapper.registerModule(new SimpleModule() {
+			@Override
+			@SuppressWarnings("unchecked")
+			public void setupModule(SetupContext context) {
+				super.setupModule(context);
+				context.addDeserializers(new Deserializers.Base() {
+					@Override
+					public JsonDeserializer<?> findEnumDeserializer(Class<?> type, DeserializationConfig config, BeanDescription beanDesc) throws JsonMappingException {
+						return new LowerEnumDeserializer((Class<Enum<?>>) type);
+					}
+				});
+			}
+		});
 
 		//Setup httpclient
 		this.httpclient = HttpClientBuilder.create()
@@ -148,12 +173,33 @@ public class StackClient {
 			StackClient client = new StackClient(authProperties.getProperty("seApiKey"));
 
 			//Get posts
-			ResponseEntry<BadgeEntry> response = client.query(UserQueries.badges(UserQueries.ME_IDS).setSite("stackoverflow"));
-			log.debug("Got " + response.getItems().size() + " badges");
-
+			ResponseEntry<AnswerEntry> response = client.query(AnswerQueries.all()
+					.setSite("stackoverflow")
+					.setFilter("!*2-Ks9DZr4MCSs67uH2q9UHUyUSATRXZkecYeRbMs"));
+			log.debug("Got " + response.getItems().size() + " entries");
 		} catch (QueryException e) {
 			e.printStackTrace();
 			System.err.println("RAW: " + e.getRawResponse());
+		}
+	}
+
+	protected static class LowerEnumDeserializer extends StdScalarDeserializer<Enum<?>> {
+		protected LowerEnumDeserializer(Class<Enum<?>> clazz) {
+			super(clazz);
+		}
+
+		@Override
+		public Enum<?> deserialize(JsonParser jp, DeserializationContext ctxt)
+				throws IOException, JsonProcessingException {
+			String text = jp.getText().toUpperCase();
+			try {
+				for (Enum curEnum : (Enum[]) getValueClass().getEnumConstants())
+					if (curEnum.name().equals(text))
+						return curEnum;
+				throw new RuntimeException("Could not find " + text + " in " + getValueClass().getName());
+			} catch (Exception e) {
+				throw new RuntimeException("Cannot deserialize enum " + getValueClass().getName() + " from " + text, e);
+			}
 		}
 	}
 }
